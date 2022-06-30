091640d7f7db93a43be8b5287ba81f9c30b8ab2f
Flexus <37239071+EnergyFlexus@users.noreply.github.com>
2022-06-30 15:34:11 +0300

    add added

---
+++ src/add.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <cstring>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#include <emscripten/bind.h>
+using namespace emscripten;
+#endif // __EMSCRIPTEN__
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+int add(int a, int b) 
+{
+    return a + b;
+}
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_BINDINGS(my_module) {
+    function("add", &add);
+}
+#endif // __EMSCRIPTEN__
