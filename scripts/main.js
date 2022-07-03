432dcf43fa63da590f6de0e0d42354b2a9b808a6
RainBowsd <alexander.nagaev52@gmail.com>
2022-07-03 14:33:47 +0300

    drag n drop refactor

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,49 @@
-import {createEvents} from './drop.js'
+import {createEvents} from './visualEvents.js'
+
+let image = document.createElement('img');
+
 /* Drag n Drop */
 
 let dropArea = document.getElementById("drop-area");
+/**
+ * @type {HTMLCanvasElement}
+ */
+const CANVAS = document.getElementById('canvas')
+
+const context = CANVAS.getContext('2d');
 
 createEvents(dropArea);
+/**
+ * @param {HTMLImageElement} image 
+ */
+let imagePreview = function drawImageOnDisplay(){
+    context.clearRect(0, 0, CANVAS.width, CANVAS.height);
+    context.drawImage(this, 0, 0, CANVAS.width, CANVAS.height);
+};
+
+/**
+ * @param {DragEvent} event 
+ */
+let handleFiles = function handleFilesFromForm(event){
+    let files = event.dataTransfer.files;
+    if(files.length > 1) {
+        console.error("Too many files!");
+        throw new Error('Only 1 file, please.');
+    }
+    let file = files.item(0);
+
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+        image.src = reader.result;
+        image.onload = imagePreview;
+    }
+}
+
+dropArea.addEventListener('drop', handleFiles, false);
+
+
 
 /*--------- */
+
 
