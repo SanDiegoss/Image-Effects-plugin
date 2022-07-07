c28ed1bb34f1a8d40e0be1be1fc53e0bd4f5c2d3
Flexus <37239071+EnergyFlexus@users.noreply.github.com>
2022-07-07 18:13:04 +0300

    fixed

--- effects-core/cpp/exported_functions.h
+++ effects-core/cpp/exported_functions.h
@@ -6,7 +6,7 @@ extern "C"
 {
 #endif // __cplusplus
 
-void cahnge_brightness(unsigned char* data, int h, int w, int brightness);
+void change_brightness(unsigned char* data, int h, int w, int brightness);
 void change_saturation(unsigned char* data, int h, int w, int saturation);
 
 #ifdef __cplusplus
--- effects-core/deploy/effects.js
+++ effects-core/deploy/effects.js
@@ -1893,10 +1893,10 @@ var asm = createWasm();
 var ___wasm_call_ctors = Module["___wasm_call_ctors"] = createExportWrapper("__wasm_call_ctors");
 
 /** @type {function(...*):?} */
-var _brightness = Module["_brightness"] = createExportWrapper("brightness");
+var _change_brightness = Module["_change_brightness"] = createExportWrapper("change_brightness");
 
 /** @type {function(...*):?} */
-var _saturation = Module["_saturation"] = createExportWrapper("saturation");
+var _change_saturation = Module["_change_saturation"] = createExportWrapper("change_saturation");
 
 /** @type {function(...*):?} */
 var _main = Module["_main"] = createExportWrapper("__main_argc_argv");
