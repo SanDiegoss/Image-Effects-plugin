31e33f4395c70662ae486c416d853d112cdb035e
RainBowsd <alexander.nagaev52@gmail.com>
2022-07-27 08:50:19 +0300

    ???????

--- effects-core/test/Slider.js
+++ effects-core/test/Slider.js
@@ -43,7 +43,7 @@ const Slider = (function() {
         thumbRadius: 6,
         max: 100,
         min: 0,
-        startValue: 0,
+        startValue: 50,
         isEnabledByDefault: false,
     };
     /**
@@ -54,135 +54,34 @@ const Slider = (function() {
         this.x = x;
         this.y = y;
     }
-    /**
-     * @param {CanvasRenderingContext2D} context
-     * @param {Settings} settings
-     * @param {Boolean} isEnabled
-     */
-    function drawBorders(context, settings, isEnabled) {
-        context.beginPath();
-
-        const LeftArcStartPoint = new Coordinates(settings.borderRadius, settings.thumbRadius - settings.height / 2);
-        const LeftArcMiddlePoint = new Coordinates(0, settings.thumbRadius);
-        const LeftArcEndPoint = new Coordinates(settings.borderRadius, settings.thumbRadius + settings.height / 2);
-
-        const RightArcStartPoint = new Coordinates(settings.width - 2 * settings.borderRadius, settings.thumbRadius - settings.height / 2);
-        const RightArcMiddlePoint = new Coordinates(settings.width, settings.thumbRadius);
-        const RightArcEndPoint = new Coordinates(settings.width - 2 * settings.borderRadius, settings.thumbRadius + settings.height / 2);
-
-        context.moveTo(LeftArcStartPoint.x, LeftArcStartPoint.y);
-        context.arcTo(LeftArcMiddlePoint.x, LeftArcMiddlePoint.y, LeftArcEndPoint.x, LeftArcEndPoint.y, settings.borderRadius);
-        context.lineTo(LeftArcEndPoint.x, LeftArcEndPoint.y);
-
-        context.moveTo(LeftArcStartPoint.x, LeftArcStartPoint.y);
-
-        context.lineTo(RightArcStartPoint.x, RightArcStartPoint.y);
-        context.arcTo(RightArcMiddlePoint.x, RightArcMiddlePoint.y, RightArcEndPoint.x, RightArcEndPoint.y, settings.borderRadius);
-        context.lineTo(RightArcEndPoint.x, RightArcEndPoint.y);
-
-        context.lineTo(LeftArcEndPoint.x, LeftArcEndPoint.y);
-
-        context.strokeStyle = (isEnabled) ? settings.borderBackgroundColor.mainColor : settings.borderBackgroundColor.disabledColor;
-        context.stroke();
-        context.fillStyle = (isEnabled) ? settings.backgroundColor.mainColor : settings.backgroundColor.disabledColor;
-        context.fill();
-
-        context.closePath();
-    }
-    /**
-     * @param {CanvasRenderingContext2D} context
-     * @param {Settings} settings
-     * @param {Boolean} isEnabled
-     */
-    function drawThumb(context, settings, isEnabled) {
-        context.beginPath();
-        context.arc(settings.thumbRadius, settings.thumbRadius, settings.thumbRadius, 0, 2*Math.PI);
-        context.fillStyle = (isEnabled) ? settings.thumbColor.mainColor : settings.thumbColor.disabledColor;
-        context.fill();
-    }
-    /**
-     * @param {CanvasRenderingContext2D} context
-     * @param {Settings} settings
-     * @param {Boolean} isEnabled
-     */
-    // function update(context, settings, isEnabled) {
-    //     context.strokeStyle = 'blue';
-    //     context.stroke();
-    //     context.fillStyle = 'red';
-    //     context.fill();
-    // }
-    /**
-     * @param {CanvasRenderingContext2D} context
-     * @param {Settings} settings
-     * @param {Boolean} isEnabled
-     * @param {Number} value
-     */
-    function drawProgress(context, settings, isEnabled, value) {
-        const total = settings.max - settings.min;
-        const percent = (value - settings.min) / total;
-        const needWidth = settings.width * percent;
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-        if (percent == 0) {
-            return;
-        }
-        const LeftArcStartPoint = new Coordinates(settings.borderRadius, settings.thumbRadius - settings.height / 2);
-        const LeftArcMiddlePoint = new Coordinates(0, settings.thumbRadius);
-        const LeftArcEndPoint = new Coordinates(settings.borderRadius, settings.thumbRadius + settings.height / 2);
-
-        const RightArcStartPoint = new Coordinates(needWidth - 2 * settings.borderRadius, settings.thumbRadius - settings.height / 2);
-        const RightArcMiddlePoint = new Coordinates(needWidth, settings.thumbRadius);
-        const RightArcEndPoint = new Coordinates(needWidth - 2 * settings.borderRadius, settings.thumbRadius + settings.height / 2);
-
-        context.beginPath();
-
-        context.moveTo(LeftArcStartPoint.x, LeftArcStartPoint.y);
-        context.arcTo(LeftArcMiddlePoint.x, LeftArcMiddlePoint.y, LeftArcEndPoint.x, LeftArcEndPoint.y, settings.borderRadius);
-        context.lineTo(LeftArcEndPoint.x, LeftArcEndPoint.y);
-
-        context.moveTo(LeftArcStartPoint.x, LeftArcStartPoint.y);
-
-        context.lineTo(RightArcStartPoint.x, RightArcStartPoint.y);
-        context.arcTo(RightArcMiddlePoint.x, RightArcMiddlePoint.y, RightArcEndPoint.x, RightArcEndPoint.y, settings.borderRadius);
-        context.lineTo(RightArcEndPoint.x, RightArcEndPoint.y);
-
-        context.lineTo(LeftArcEndPoint.x, LeftArcEndPoint.y);
-
-        context.strokeStyle = (isEnabled) ? settings.borderProgressColor.mainColor : settings.borderProgressColor.disabledColor;
-        context.stroke();
-        context.fillStyle = (isEnabled) ? settings.progressColor.mainColor : settings.progressColor.disabledColor;
-        context.fill();
-        context.closePath();
-    }
     return {
         /**
          * Constructor for sliders
          * @param {Settings} settings
          */
         Slider: function(settings) {
-            this.settings = settings || defualtSliderSettings;
-            settings = this.settings;
-            this.isEnabled = this.settings.isEnabledByDefault;
-            if (this.settings.borderRadius > this.settings.height / 2) {
-                this.settings.borderRadius = this.settings.height / 2;
+            settings = settings || defualtSliderSettings;
+            let isEnabled = settings.isEnabledByDefault;
+            if (settings.borderRadius > settings.height / 2) {
+                settings.borderRadius = settings.height / 2;
             }
-            if (this.settings.thumbRadius*2 < this.settings.height) {
-                this.settings.thumbRadius = this.settings.height / 2;
+            if (settings.thumbRadius*2 < settings.height) {
+                settings.thumbRadius = settings.height / 2;
             }
 
-            this.value = this.settings.startValue;
-            let value = this.value;
+            let value = settings.startValue;
 
             const backgoundCanvas = document.createElement('canvas');
             const progressCanvas = document.createElement('canvas');
             const thumbCanvas = document.createElement('canvas');
 
-            backgoundCanvas.width = this.settings.width;
-            progressCanvas.width = this.settings.width;
-            thumbCanvas.width = this.settings.thumbRadius * 2;
+            backgoundCanvas.width = settings.width;
+            progressCanvas.width = settings.width;
+            thumbCanvas.width = settings.thumbRadius * 2;
 
-            backgoundCanvas.height = this.settings.thumbRadius * 2;
-            progressCanvas.height = this.settings.thumbRadius * 2;
-            thumbCanvas.height = this.settings.thumbRadius * 2;
+            backgoundCanvas.height = settings.thumbRadius * 2;
+            progressCanvas.height = settings.thumbRadius * 2;
+            thumbCanvas.height = settings.thumbRadius * 2;
 
             backgoundCanvas.style.zIndex = 1;
             progressCanvas.style.zIndex = 2;
@@ -194,8 +93,96 @@ const Slider = (function() {
 
             const bgContext = backgoundCanvas.getContext('2d');
             const pgContext = progressCanvas.getContext('2d');
-            drawBorders(bgContext, settings, this.isEnabled);
-            drawThumb(thumbCanvas.getContext('2d'), settings, this.isEnabled);
+            const thContext = thumbCanvas.getContext('2d');
+            /**
+             */
+            function drawBorders() {
+                bgContext.beginPath();
+
+                const LeftArcStartPoint = new Coordinates(settings.borderRadius, settings.thumbRadius - settings.height / 2);
+                const LeftArcMiddlePoint = new Coordinates(0, settings.thumbRadius);
+                const LeftArcEndPoint = new Coordinates(settings.borderRadius, settings.thumbRadius + settings.height / 2);
+
+                const RightArcStartPoint = new Coordinates(settings.width - 2 * settings.borderRadius, settings.thumbRadius - settings.height / 2);
+                const RightArcMiddlePoint = new Coordinates(settings.width, settings.thumbRadius);
+                const RightArcEndPoint = new Coordinates(settings.width - 2 * settings.borderRadius, settings.thumbRadius + settings.height / 2);
+
+                bgContext.moveTo(LeftArcStartPoint.x, LeftArcStartPoint.y);
+                bgContext.arcTo(LeftArcMiddlePoint.x, LeftArcMiddlePoint.y, LeftArcEndPoint.x, LeftArcEndPoint.y, settings.borderRadius);
+                bgContext.lineTo(LeftArcEndPoint.x, LeftArcEndPoint.y);
+
+                bgContext.moveTo(LeftArcStartPoint.x, LeftArcStartPoint.y);
+
+                bgContext.lineTo(RightArcStartPoint.x, RightArcStartPoint.y);
+                bgContext.arcTo(RightArcMiddlePoint.x, RightArcMiddlePoint.y, RightArcEndPoint.x, RightArcEndPoint.y, settings.borderRadius);
+                bgContext.lineTo(RightArcEndPoint.x, RightArcEndPoint.y);
+
+                bgContext.lineTo(LeftArcEndPoint.x, LeftArcEndPoint.y);
+
+                bgContext.strokeStyle = (isEnabled) ? settings.borderBackgroundColor.mainColor : settings.borderBackgroundColor.disabledColor;
+                bgContext.stroke();
+                bgContext.fillStyle = (isEnabled) ? settings.backgroundColor.mainColor : settings.backgroundColor.disabledColor;
+                bgContext.fill();
+
+                bgContext.closePath();
+            }
+            /**
+             */
+            function drawThumb() {
+                thContext.clearRect(0, 0, thumbCanvas.width, thumbCanvas.height);
+                thContext.beginPath();
+                thContext.arc(settings.thumbRadius, settings.thumbRadius, settings.thumbRadius, 0, 2*Math.PI);
+                thContext.fillStyle = (isEnabled) ? settings.thumbColor.mainColor : settings.thumbColor.disabledColor;
+                thContext.fill();
+                const total = settings.max - settings.min;
+                const percent = (value + settings.min) / total;
+                const needWidth = (settings.width - settings.thumbRadius*2) * percent;
+                thumbCanvas.style.left = needWidth + 'px';
+            }
+            /**
+             */
+            function drawProgress() {
+                const total = settings.max - settings.min;
+                const percent = (value + settings.min) / total;
+                const needWidth = (settings.width - settings.thumbRadius) * percent;
+                console.log(needWidth);
+                pgContext.clearRect(0, 0, progressCanvas.width, progressCanvas.height);
+                if (percent == 0) {
+                    return;
+                }
+                const LeftArcStartPoint = new Coordinates(settings.borderRadius, settings.thumbRadius - settings.height / 2);
+                const LeftArcMiddlePoint = new Coordinates(0, settings.thumbRadius);
+                const LeftArcEndPoint = new Coordinates(settings.borderRadius, settings.thumbRadius + settings.height / 2);
+
+                const RightArcStartPoint = new Coordinates(needWidth + settings.thumbRadius / 2, settings.thumbRadius - settings.height / 2);
+                const RightArcEndPoint = new Coordinates(needWidth + settings.thumbRadius / 2, settings.thumbRadius + settings.height / 2);
+
+                pgContext.beginPath();
+
+                pgContext.moveTo(LeftArcStartPoint.x, LeftArcStartPoint.y);
+                pgContext.arcTo(LeftArcMiddlePoint.x, LeftArcMiddlePoint.y, LeftArcEndPoint.x, LeftArcEndPoint.y, settings.borderRadius);
+                pgContext.lineTo(LeftArcEndPoint.x, LeftArcEndPoint.y);
+
+                pgContext.moveTo(LeftArcStartPoint.x, LeftArcStartPoint.y);
+
+                pgContext.lineTo(RightArcStartPoint.x, RightArcStartPoint.y);
+                pgContext.lineTo(RightArcEndPoint.x, RightArcEndPoint.y);
+
+                pgContext.lineTo(LeftArcEndPoint.x, LeftArcEndPoint.y);
+
+                pgContext.strokeStyle = (isEnabled) ? settings.borderProgressColor.mainColor : settings.borderProgressColor.disabledColor;
+                pgContext.stroke();
+                pgContext.fillStyle = (isEnabled) ? settings.progressColor.mainColor : settings.progressColor.disabledColor;
+                pgContext.fill();
+                pgContext.closePath();
+            }
+            /**
+             */
+            (function update() {
+                drawBorders();
+                drawProgress();
+                drawThumb();
+            })();
             thumbCanvas.onpointerdown = function(event) {
                 /**
                  * @type {HTMLCanvasElement}
@@ -220,10 +207,11 @@ const Slider = (function() {
                         const total = settings.max - settings.min;
                         const maxleft = progressCanvas.width - thumbCanvas.width;
                         const percent = moving / maxleft;
-                        // TODO: values,refactoring, fix progressbar
-                        value = (percent * 100);
+                        // TODO: refactoring
+                        value = (settings.min + percent * total);
+                        value = Math.floor(value);
                     }
-                    console.log(value);
+                    progressCanvas.setAttribute('value', '' + value);
                     drawProgress(pgContext, settings, true, value);
                 }
                 /**
@@ -242,18 +230,31 @@ const Slider = (function() {
                 document.addEventListener('pointermove', onPointerMove, false);
                 document.addEventListener('pointerup', onPointerUp, false);
             };
-           // update(this.bgContext);
-            // TODO: Зависимость от прогресса
             /**
              * @param {HTMLDivElement} div
              */
             this.addToDiv = function(div) {
                 div.style.position = 'relative';
-                div.style.height = this.settings.thumbRadius * 2 + 'px';
+                div.style.height = settings.thumbRadius * 2 + 'px';
                 div.style.margin = '2px';
                 div.appendChild(backgoundCanvas);
                 div.appendChild(progressCanvas);
                 div.appendChild(thumbCanvas);
+            };
+            this.getValue = function() {
+                return value;
+            };
+            this.setValue = function(oValue) {
+                value = oValue;
+                update();
+            };
+            this.enable = function() {
+                isEnabled = true;
+                update();
+            };
+            this.disable = function() {
+                isEnabled = false;
+                update();
             };
         },
     };
