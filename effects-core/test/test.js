e14d3ae47cff28c2374b1d7061d91f59627f9c4b
RainBowsd <alexander.nagaev52@gmail.com>
2022-07-27 10:16:59 +0300

    add tests

---
+++ effects-core/test/test.js
@@ -0,0 +1,8 @@
+
+const slider = new SliderModule.CSlider();
+const div = document.getElementById('content');
+slider.addToDiv(div);
+const div2 = document.getElementById('content2');
+const slider2 = new SliderModule.CSlider();
+slider2.addToDiv(div2);
+
