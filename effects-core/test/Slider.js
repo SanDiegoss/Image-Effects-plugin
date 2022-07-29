66e3f1d0ef8fa7eac9cb6ab5fe06a85c74dced18
RainBowsd <alexander.nagaev52@gmail.com>
2022-07-29 12:13:30 +0300

    global fixes

--- effects-core/test/Slider.js
+++ effects-core/test/Slider.js
@@ -20,6 +20,7 @@
  * @property {Number} min
  * @property {Number} startValue
  * @property {Boolean} isEnabledByDefault
+ * @property {Boolean} isVertical
  */
 /**
  * Slider module
@@ -33,9 +34,9 @@ const SliderModule = (function() {
     const defaultSliderSettings = {
         backgroundColor: {mainColor: '#c0c0c0', disabledColor: '#c0c0c0'},
         progressColor: {mainColor: '#444444', disabledColor: '#a0a0a0'},
-        borderProgressColor: {mainColor: '#444444', disabledColor: 'red'},
+        borderProgressColor: {mainColor: '#444444', disabledColor: 'blue'},
         thumbColor: {mainColor: '#444444', disabledColor: '#a0a0a0'},
-        borderBackgroundColor: {mainColor: '#c0c0c0', disabledColor: 'blue'},
+        borderBackgroundColor: {mainColor: '#c0c0c0', disabledColor: 'red'},
         borderRadius: 2,
         thumbRadius: 6,
         height: 4,
@@ -43,6 +44,7 @@ const SliderModule = (function() {
         min: -100,
         startValue: 0,
         isEnabledByDefault: false,
+        isVertical: false,
     };
     /**
      * @param {Number} x
@@ -69,11 +71,9 @@ const SliderModule = (function() {
         CSlider: function(oSettings, parent) {
             // TODO: refactoring
             const settings = {};
-            // eslint-disable-next-line prefer-const
+            // eslint-disable-next-line prefer-const, guard-for-in
             for (let i in defaultSliderSettings) {
-                if (Object.prototype.hasOwnProperty.call(defaultSliderSettings, i)) {
-                    settings[i] = oSettings[i] || defaultSliderSettings[i];
-                }
+                settings[i] = oSettings[i] || defaultSliderSettings[i];
             }
             let isEnabled = settings.isEnabledByDefault;
             if (settings.borderRadius > settings.height / 2) {
@@ -89,117 +89,94 @@ const SliderModule = (function() {
             const context = canvas.getContext('2d');
             /**
              */
-            function drawBorders() {
+            function update() {
+                const cWidth = (settings.isVertical) ? canvas.height : canvas.width;
+                const cHeight = (settings.isVertical) ? canvas.width : canvas.height;
                 const borderRadius = settings.borderRadius * window.devicePixelRatio;
                 const height = settings.height * window.devicePixelRatio;
                 const thumbRadius = settings.thumbRadius * window.devicePixelRatio;
-                context.beginPath();
+                const total = settings.max - settings.min;
+                const percent = (value - settings.min) / total;
+                const needx = ((cWidth - 2 * thumbRadius) * percent) + thumbRadius;
+
                 context.clearRect(0, 0, canvas.width, canvas.height);
-                const LeftArcStartPoint = new Coordinates(borderRadius, thumbRadius - height / 2);
-                const LeftArcMiddlePoint = new Coordinates(0, thumbRadius);
-                const LeftArcEndPoint = new Coordinates(borderRadius, thumbRadius + height / 2);
 
-                const RightArcStartPoint = new Coordinates(canvas.width - 2 * borderRadius, thumbRadius - height / 2);
-                const RightArcMiddlePoint = new Coordinates(canvas.width, thumbRadius);
-                const RightArcEndPoint = new Coordinates(canvas.width - 2 * borderRadius, thumbRadius + height / 2);
+                const leftTop = new Coordinates(borderRadius * 2, cHeight / 2 - height / 2);
+                const leftMid = new Coordinates(0, cHeight / 2);
+                const leftBot = new Coordinates(borderRadius * 2, cHeight / 2 + height / 2);
 
-                context.moveTo(LeftArcStartPoint.x, LeftArcStartPoint.y);
-                context.arcTo(LeftArcMiddlePoint.x, LeftArcMiddlePoint.y, LeftArcEndPoint.x, LeftArcEndPoint.y, borderRadius);
-                context.lineTo(LeftArcEndPoint.x, LeftArcEndPoint.y);
+                const centerTop = new Coordinates(needx, cHeight / 2 - height / 2);
+                const centerBot = new Coordinates(needx, cHeight / 2 + height / 2);
 
-                context.moveTo(LeftArcStartPoint.x, LeftArcStartPoint.y);
+                const rightTop = new Coordinates(cWidth - 2 * borderRadius, cHeight / 2 - height / 2);
+                const rightMid = new Coordinates(cWidth, cHeight / 2);
+                const rightBot = new Coordinates(cWidth - 2 * borderRadius, cHeight / 2 + height / 2);
 
-                context.lineTo(RightArcStartPoint.x, RightArcStartPoint.y);
-                context.arcTo(RightArcMiddlePoint.x, RightArcMiddlePoint.y, RightArcEndPoint.x, RightArcEndPoint.y, borderRadius);
-                context.lineTo(RightArcEndPoint.x, RightArcEndPoint.y);
-
-                context.lineTo(LeftArcEndPoint.x, LeftArcEndPoint.y);
-
-                context.strokeStyle = (isEnabled) ? settings.borderBackgroundColor.mainColor : settings.borderBackgroundColor.disabledColor;
-                context.stroke();
-                context.fillStyle = (isEnabled) ? settings.backgroundColor.mainColor : settings.backgroundColor.disabledColor;
-                context.fill();
-
-                context.closePath();
-            }
-            /**
-             */
-            function drawThumb() {
-                const thumbRadius = settings.thumbRadius * window.devicePixelRatio;
-                const total = settings.max - settings.min;
-                const percent = (value - settings.min) / total;
-                const needx = ((canvas.width - 2 * thumbRadius) * percent) + thumbRadius;
-                context.beginPath();
-                context.arc(needx, thumbRadius, thumbRadius, 0, 2*Math.PI);
-                context.fillStyle = (isEnabled) ? settings.thumbColor.mainColor : settings.thumbColor.disabledColor;
-                context.fill();
-                context.closePath();
-            }
-            /**
-             */
-            function drawProgress() {
-                const borderRadius = settings.borderRadius * window.devicePixelRatio;
-                const height = settings.height * window.devicePixelRatio;
-                const thumbRadius = settings.thumbRadius * window.devicePixelRatio;
-                const total = settings.max - settings.min;
-                const percent = (value - settings.min) / total;
-                const needWidth = (canvas.width - thumbRadius) * percent;
-                console.log(needWidth);
-                if (percent == 0) {
-                    return;
+                const thumbCoords = new Coordinates(needx, cHeight / 2);
+                if (settings.isVertical) {
+                    [leftTop, leftMid, leftBot, centerTop, centerBot, rightTop, rightMid, rightBot, thumbCoords].forEach(function(item) {
+                        item.y = [cWidth - item.x, item.x = item.y][0];
+                    });
                 }
-                const LeftArcStartPoint = new Coordinates(borderRadius, thumbRadius - height / 2);
-                const LeftArcMiddlePoint = new Coordinates(0, thumbRadius);
-                const LeftArcEndPoint = new Coordinates(borderRadius, thumbRadius + height / 2);
+                const needPenW = 1;
+                context.lineWidth = (needPenW * window.devicePixelRatio) >> 0;
+                const offsetCeil = (context.lineWidth & 1) / 2;
 
-                const RightArcStartPoint = new Coordinates(needWidth + thumbRadius / 2, thumbRadius - height / 2);
-                const RightArcEndPoint = new Coordinates(needWidth + thumbRadius / 2, thumbRadius + height / 2);
-
+                // before thumb
                 context.beginPath();
-
-                context.moveTo(LeftArcStartPoint.x, LeftArcStartPoint.y);
-                context.arcTo(LeftArcMiddlePoint.x, LeftArcMiddlePoint.y, LeftArcEndPoint.x, LeftArcEndPoint.y, borderRadius);
-                context.lineTo(LeftArcEndPoint.x, LeftArcEndPoint.y);
-
-                context.moveTo(LeftArcStartPoint.x, LeftArcStartPoint.y);
-
-                context.lineTo(RightArcStartPoint.x, RightArcStartPoint.y);
-                context.lineTo(RightArcEndPoint.x, RightArcEndPoint.y);
-
-                context.lineTo(LeftArcEndPoint.x, LeftArcEndPoint.y);
-
-                context.strokeStyle = (isEnabled) ? settings.borderProgressColor.mainColor : settings.borderProgressColor.disabledColor;
-                context.stroke();
+                context.moveTo(leftTop.x >> 0, leftTop.y >> 0 + offsetCeil);
+                context.arcTo(leftMid.x >> 0, leftMid.y >> 0 + offsetCeil, leftBot.x >> 0, leftBot.y >> 0 + offsetCeil, borderRadius);
+                context.lineTo(leftBot.x >> 0, leftBot.y >> 0 + offsetCeil);
+                context.lineTo(centerBot.x >> 0, centerBot.y >> 0 + offsetCeil);
+                context.lineTo(centerTop.x >> 0, centerTop.y >> 0 + offsetCeil);
+                context.lineTo(leftTop.x >> 0, leftTop.y >> 0 + offsetCeil);
+                context.arcTo(leftMid.x >> 0, leftMid.y >> 0 + offsetCeil, leftBot.x >> 0, leftBot.y >> 0 + offsetCeil, borderRadius);
                 context.fillStyle = (isEnabled) ? settings.progressColor.mainColor : settings.progressColor.disabledColor;
                 context.fill();
-                context.closePath();
-            }
-            /**
-             */
-            function update() {
-                drawBorders();
-                drawProgress();
-                drawThumb();
+                context.strokeStyle = (isEnabled) ? settings.borderProgressColor.mainColor : settings.borderProgressColor.disabledColor;
+                context.stroke();
+                // after thumb
+                context.beginPath();
+                context.moveTo(centerTop.x >> 0, centerTop.y >> 0 + offsetCeil);
+                context.lineTo(rightTop.x >> 0, rightTop.y >> 0 + offsetCeil);
+                context.arcTo(rightMid.x >> 0, rightMid.y >> 0 + offsetCeil, rightBot.x >> 0, rightBot.y >> 0 + offsetCeil, borderRadius);
+                context.lineTo(rightBot.x >> 0, rightBot.y >> 0 + offsetCeil);
+                context.lineTo(centerBot.x >> 0, centerBot.y >> 0 + offsetCeil);
+                context.fillStyle = (isEnabled) ? settings.backgroundColor.mainColor : settings.backgroundColor.disabledColor;
+                context.fill();
+                context.strokeStyle = (isEnabled) ? settings.borderBackgroundColor.mainColor : settings.borderBackgroundColor.disabledColor;
+                context.stroke();
+                // Thumb
+                context.beginPath();
+                context.arc(thumbCoords.x, thumbCoords.y, thumbRadius, 0, 2*Math.PI);
+                context.fillStyle = (isEnabled) ? settings.thumbColor.mainColor : settings.thumbColor.disabledColor;
+                context.fill();
             }
             canvas.onpointerdown = function(event) {
                 event.preventDefault();
                 event.stopPropagation();
-                moveAt(event.pageX);
+                const pageX = (settings.isVertical) ? event.pageY : event.pageX;
+                moveAt(pageX);
                 /**
                  * @param {Number} pageX
                  */
                 function moveAt(pageX) {
-                    const offset = parent.getBoundingClientRect();
-                    const moving = (pageX - offset.left - settings.thumbRadius + window.pageXOffset);
-                    console.log(offset.left);
+                    const rect = parent.getBoundingClientRect();
+                    const offset = (settings.isVertical) ? rect.top : rect.left;
+                    const oWidth = (settings.isVertical) ? rect.height : rect.width;
+                    const pageOffset = (settings.isVertical) ? window.pageYOffset : window.pageXOffset;
+                    let moving = (pageX - offset - settings.thumbRadius + pageOffset);
+                    if (settings.isVertical) {
+                        moving = oWidth - moving - 2*settings.thumbRadius;
+                    }
                     if (moving < 0) {
                         value = settings.min;
-                    } else if (moving > offset.width - settings.thumbRadius * 2) {
+                    } else if (moving > oWidth - settings.thumbRadius * 2) {
                         value = settings.max;
                     } else {
                         const total = settings.max - settings.min;
-                        const maxleft = offset.width - settings.thumbRadius * 2;
-                        const percent = moving / maxleft;
+                        const maxWidth = oWidth - settings.thumbRadius * 2;
+                        const percent = moving / maxWidth;
                         value = (settings.min + percent * total);
                         // value = Math.floor(value);
                     }
@@ -210,7 +187,8 @@ const SliderModule = (function() {
                  * @param {PointerEvent} event
                  */
                 function onPointerMove(event) {
-                    moveAt(event.pageX);
+                    const pageX = (settings.isVertical) ? event.pageY : event.pageX;
+                    moveAt(pageX);
                 }
                 /**
                  * @param {PointerEvent} event
@@ -243,12 +221,17 @@ const SliderModule = (function() {
             parent.appendChild(canvas);
             resize();
             window.addEventListener('resize', resize);
+            if (typeof ResizeObserver !== 'undefined') {
+                const ro = new ResizeObserver(function() {
+                    resize();
+                });
+                ro.observe(parent);
+            }
             /**
              */
             function resize() {
                 const scale = window.devicePixelRatio;
                 const rect = parent.getBoundingClientRect();
-                console.log(rect.height);
                 canvas.style.width = rect.width + 'px';
                 canvas.style.height = rect.height + 'px';
                 canvas.width = Math.round(scale * rect.right) - Math.round(scale * rect.left);
--- effects-core/test/test.js
+++ effects-core/test/test.js
@@ -1,9 +1,30 @@
+/* eslint-disable max-len */
+/* eslint-disable indent */
 
-const div = document.getElementById('content');
+const divs = document.querySelectorAll('.cont');
+const vert = document.querySelector('.vert');
+const sliders = [];
+const colors = ['blue', 'red', 'black', 'yellow', 'green', 'purple', 'orange', 'aqua', 'yellowgreen'];
+SliderModule.changeDefaultSettings({
+    borderRadius: 10,
+    thumbRadius: 18,
+    height: 15,
+});
+let i = 0;
+Array.prototype.forEach.call(divs, function(div) {
+    sliders.push(new SliderModule.CSlider({
+        progressColor: {
+            mainColor: colors[i], disabledColor: colors[i+1],
+        },
+        thumbColor: {
+            mainColor: colors[i+1], disabledColor: colors[i+2],
+        },
+        borderRadius: (i+5)*2,
+        height: (i+5)*2,
+        thumbRadius: (i+5)*2,
+    }, div));
+    console.log(i);
+    i+=1;
+});
 
-// eslint-disable-next-line no-unused-vars
-const slider = new SliderModule.CSlider({}, div);
-// slider.setValue(0);
-// const div2 = document.getElementById('content2');
-// const slider2 = new SliderModule.CSlider({}, div2);
-
+new SliderModule.CSlider({isVertical: true}, vert);
